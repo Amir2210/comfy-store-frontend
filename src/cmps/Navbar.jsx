0aefac154d4f719a8e92b696bb30d3bfe59b2de7
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-19 13:13:22 +0200

    Create custom class align-elemets

--- src/cmps/Navbar.jsx
+++ src/cmps/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 export function Navbar() {
   return (
-    <nav className='bg-slate-900'>
-      <div className='text-white flex justify-between'>
-        <div>
+    <nav className='bg-slate-900 '>
+      <div className='text-white flex justify-between align-elemets'>
+        <div className=''>
           <Link>C</Link>
         </div>
         <div>
