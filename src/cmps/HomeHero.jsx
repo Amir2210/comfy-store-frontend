aff4293c18e4c4bfc7e60edc42ac0f02a7a62968
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-20 11:03:51 +0200

    Add HeroCarousel and complete the hero section

--- src/cmps/HomeHero.jsx
+++ src/cmps/HomeHero.jsx
@@ -1,13 +1,15 @@
+import { HeroCarousel } from './HeroCarousel';
+
 export function HomeHero() {
   return (
-    <section className='align-elemets grid grid-cols-2 gap-24 py-20'>
-      <div>
-        <h1 className='text-6xl font-bold mb-6'>We are changing the way people shop</h1>
+    <section className='align-elemets flex py-10 lg:grid lg:grid-cols-2 lg:gap-24 lg:py-20 '>
+      <div className=''>
+        <h1 className='font-bold text-4xl mb-6 lg:text-6xl '>We are changing the way people shop</h1>
         <p className='mb-6 text-lg tracking-wider'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellat explicabo enim soluta temporibus asperiores aut obcaecati perferendis porro nobis.</p>
         <button className='btn btn-primary capitalize'>our products</button>
       </div>
-      <div>
-        <img className='w-36' src="src/assets/imgs/hero1.webp" alt="" />
+      <div className='hidden lg:max-h-96 lg:flex'>
+        <HeroCarousel />
       </div>
     </section>
   )
