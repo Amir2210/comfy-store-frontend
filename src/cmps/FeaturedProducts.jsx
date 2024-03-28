5d7e32f53aa0441b65a174d6ddbd86d083dbeac1
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-28 11:30:05 +0200

    Render the FeaturedProductsPreview cmp

--- src/cmps/FeaturedProducts.jsx
+++ src/cmps/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
 import { loadFeaturedProducts } from '../store/actions/featuredProducts.action'
 import { useEffect } from 'react'
+import { FeaturedProductsPreview } from './FeaturedProductsPreview'
 export function FeaturedProducts() {
   const featuredProducts = useSelector((storeState) => storeState.featuredProductsModule.featuredProducts)
   console.log('featuredProducts:', featuredProducts)
@@ -15,6 +16,9 @@ export function FeaturedProducts() {
     <section className='align-elemets mt-6'>
       <h1 className='text-bold text-4xl'>Featured Products</h1>
       <div className='w-full h-1 bg-secondary mt-4'></div>
+      <div className='pt-12 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
+        {featuredProducts.map(product => <FeaturedProductsPreview key={product._id} product={product} />)}
+      </div>
     </section>
   )
 }
