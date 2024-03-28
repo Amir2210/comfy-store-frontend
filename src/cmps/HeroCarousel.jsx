feba80fab2fe344086f10436b61d68018fde5a46
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-28 12:32:31 +0200

    Change the Carousel

--- src/cmps/HeroCarousel.jsx
+++ src/cmps/HeroCarousel.jsx
@@ -1,34 +1,97 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 export function HeroCarousel() {
   return (
-    <div className="carousel w-full rounded-lg">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src="src/assets/imgs/hero1.webp" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src="src/assets/imgs/hero2.webp" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src="src/assets/imgs/hero3.webp" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide4" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src="src/assets/imgs/hero4.webp" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-    </div>
+    <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container rounded-lg"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024
+          },
+          items: 1
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0
+          },
+          items: 1
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464
+          },
+          items: 1
+        }
+      }}
+      rewind={false}
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={false}
+      sliderClass=""
+      slidesToSlide={1}
+      swipeable
+    >
+      <img
+        src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1711618649/comfy-store/pob7wpkxpj2c3m37pj4i.webp"
+        style={{
+          display: 'block',
+          height: '100%',
+          margin: 'auto',
+          width: '100%',
+          borderRadius: '0.5rem'
+        }}
+      />
+      <img
+        src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1711618656/comfy-store/bob2bohwob6t0ero3ibv.webp"
+        style={{
+          display: 'block',
+          height: '100%',
+          margin: 'auto',
+          width: '100%',
+          borderRadius: '0.5rem'
+        }}
+      />
+      <img
+        src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1711618653/comfy-store/o2b2ha7gngavn8cforlp.jpg"
+        style={{
+          display: 'block',
+          height: '100%',
+          margin: 'auto',
+          width: '100%',
+          borderRadius: '0.5rem'
+        }}
+      />
+      <img
+        src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1711618842/comfy-store/ezwkit7fumi1p5h9rabl.webp"
+        style={{
+          display: 'block',
+          height: '100%',
+          margin: 'auto',
+          width: '100%',
+          borderRadius: '0.5rem'
+        }}
+      />
+    </Carousel>
   )
 }
--- src/cmps/HomeHero.jsx
+++ src/cmps/HomeHero.jsx
@@ -1,5 +1,6 @@
 import { HeroCarousel } from './HeroCarousel';
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 export function HomeHero() {
   return (
     <section className='align-elemets flex py-10 lg:grid lg:grid-cols-2 lg:gap-24 lg:py-20 '>
@@ -8,7 +9,7 @@ export function HomeHero() {
         <p className='mb-6 text-lg tracking-wider'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellat explicabo enim soluta temporibus asperiores aut obcaecati perferendis porro nobis.</p>
         <button className='btn btn-primary capitalize'>our products</button>
       </div>
-      <div className='hidden lg:max-h-96 lg:flex'>
+      <div className='hidden lg:max-h-96 lg:flex rounded-lg'>
         <HeroCarousel />
       </div>
     </section>
