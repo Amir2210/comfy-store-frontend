f428d138e9191c8529bd52606a500b083f4e69f1
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-04-02 16:09:37 +0300

    render products length and to buttons

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,18 @@
 import { useSelector } from 'react-redux'
 import { Navbar } from '../cmps/Navbar'
 import { loadProducts } from '../store/actions/products.action'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+//ICONS
+import { IoGrid } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
 export function Products() {
   const products = useSelector((storeState) => storeState.productsModule.products)
   const isLoading = useSelector((storeState) => storeState.productsModule.isLoading)
-  console.log(isLoading)
+  const [layout, setLayout] = useState('grid')
+
   useEffect(() => {
     loadProducts()
       .catch((err) => {
@@ -16,9 +23,15 @@ export function Products() {
     <>
       <Navbar />
       <section className='align-elemets mt-6'>
-        <div className='flex'>
-          <div>{products.length} products</div>
+        <div className='flex justify-between'>
+          <h2 className='text-xl font-medium'>{!isLoading && products.length} {products.length <= 1 ? 'product' : 'products'}</h2>
+          <div>
+            <button onClick={() => setLayout('grid')} className={`text-xl btn btn-circle btn-sm mr-2 ${layout === 'grid' && 'btn-primary'}`}><IoGrid /></button>
+            <button onClick={() => setLayout('list')} className={`text-xl btn btn-circle btn-sm ${layout === 'list' && 'btn-primary'}`}><RxHamburgerMenu /></button>
+          </div>
         </div>
+        <div className='w-full h-1 bg-secondary mt-4'></div>
+
       </section>
     </>
   )
