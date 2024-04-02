6d1a44250cf48dff9a4d4a618163b4593ebcc8c3
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-04-02 13:18:41 +0300

    Add loader while waiting for the FeaturedProducts Req

--- src/cmps/FeaturedProducts.jsx
+++ src/cmps/FeaturedProducts.jsx
@@ -2,9 +2,10 @@ import { useSelector } from 'react-redux'
 import { loadFeaturedProducts } from '../store/actions/featuredProducts.action'
 import { useEffect } from 'react'
 import { FeaturedProductsPreview } from './FeaturedProductsPreview'
+import { RotatingTriangles } from 'react-loader-spinner'
 export function FeaturedProducts() {
   const featuredProducts = useSelector((storeState) => storeState.featuredProductsModule.featuredProducts)
-
+  const isLoading = useSelector((storeState) => storeState.featuredProductsModule.isLoading)
   useEffect(() => {
     loadFeaturedProducts()
       .catch((err) => {
@@ -16,7 +17,16 @@ export function FeaturedProducts() {
       <h1 className='text-bold text-4xl'>Featured Products</h1>
       <div className='w-full h-1 bg-secondary mt-4'></div>
       <div className='pt-12 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
-        {featuredProducts.map(product => <FeaturedProductsPreview key={product._id} product={product} />)}
+        {!isLoading && featuredProducts.map(product => <FeaturedProductsPreview key={product._id} product={product} />)}
+        {isLoading && <RotatingTriangles
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="rotating-triangles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />}
       </div>
     </section>
   )
