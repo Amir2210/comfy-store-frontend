88bc7debf9b11c5edd88eaa4f3c5aec20ced5bba
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-04-09 10:23:32 +0300

    Add the userModule to the store of the app

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,11 @@ import { combineReducers, createStore } from 'redux'
 
 import { featuredProductsReducer } from './reducers/featuredProducts.reducer'
 import { productsReducer } from './reducers/products.reducer'
-
+import { userReducer } from "./reducers/user.reducer.js"
 const rootReducer = combineReducers({
   featuredProductsModule: featuredProductsReducer,
-  productsModule: productsReducer
+  productsModule: productsReducer,
+  userModule: userReducer,
 })
 
 
