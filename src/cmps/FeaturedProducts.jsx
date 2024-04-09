951f97e0e371e1c8da4c28d68ff230885ef4223b
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-04-09 13:55:16 +0300

    Change the margin in the FeaturedProducts cmp

--- src/cmps/FeaturedProducts.jsx
+++ src/cmps/FeaturedProducts.jsx
@@ -16,7 +16,7 @@ export function FeaturedProducts() {
       })
   }, [])
   return (
-    <section className='align-elemets mt-6'>
+    <section className='align-elemets my-6'>
       <h1 className='text-bold text-4xl'>Featured Products</h1>
       <div className='w-full h-1 bg-secondary mt-4'></div>
       <div className='pt-12 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
